aee2aeffbd79e4be8d81df3b9e65cb4a0c4da551
arya anand <aryaanand1000@gmail.com>
2020-10-01 11:25:08 +0530

    floor working

--- geometrydash.py
+++ geometrydash.py
@@ -10,17 +10,25 @@ clock = pygame.time.Clock()
 
 FONT = pygame.font.SysFont('comicsans', 40)
 
+groundSurface = pygame.image.load('assets/ground.png')
+
 class PLayer:
     def __init__(self):
         pass
 
-
+x = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-
+    if x <= -WIDTH:
+        x = 0
+    else:
+        x -= 3
+    
+    screen.blit(groundSurface, (x, 50))
+    screen.blit(groundSurface,(x + WIDTH, 50))
     pygame.display.update()
     clock.tick(60)
