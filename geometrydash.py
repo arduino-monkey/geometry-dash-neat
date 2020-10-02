d05eab1171f095f5bc3436af2f3e0514d55c03af
arya anand <aryaanand1000@gmail.com>
2020-10-02 21:07:25 +0530

    base working

--- geometrydash.py
+++ geometrydash.py
@@ -24,35 +24,44 @@ class Base:
 class Player:
     surface = pygame.image.load('assets/player.png')
     x = 50
-    gravity = 0.25
-
+    gravity = 0.5
     def __init__(self, y):
         self.y = y
         self.rect = Player.surface.get_rect(bottomleft = (Player.x, self.y))
         self.movement = 0
-
+        self.jumped = False
     def move(self):
         if self.rect.bottomleft[1] <= Base.y:
             self.movement += Player.gravity
             self.rect.centery += self.movement
-    
+
     def draw(self, screen):
-        print(self.rect)
         screen.blit(Player.surface, self.rect)
 
     def jump(self):
-        self.movement = 0
-        self.movement -= 5
+        if self.rect.bottomleft[1] >= Base.y:
+            self.movement = 0
+            self.movement -= 10
+            self.rect.centery += self.movement
+        
 class Enemy:
     surface = pygame.image.load('assets/enemy.png')
     y = Base.y
+    vel = 3
+    
     def __init__(self, x):
         self.x = x
         self.rect = Enemy.surface.get_rect(bottomleft=(self.y, Enemy.x))
+    
+    def move(self):
+        self.x -= Enemy.vel
+    
+    def draw(self):
+        screen.blit(Enemy.surface, self.rect)
 
 def main():
     b = Base()
-    p = Player(100)
+    p = Player(Base.y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -62,10 +71,11 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     p.jump()
+                
         screen.fill((0,0,0))
         p.move()
-        p.draw(screen)
         b.draw(screen)
+        p.draw(screen)
         pygame.display.flip()
         clock.tick(100)
 
